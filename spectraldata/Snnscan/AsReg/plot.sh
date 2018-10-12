0876df963f08b92009ed3e2fe8167b0439f0dc4a
David Lafferty <lafferty@thphys.uni-heidelberg.de>
2018-10-12 23:55:04 +0200

    Update

--- spectraldata/Snnscan/AsReg/plot.sh
+++ spectraldata/Snnscan/AsReg/plot.sh
@@ -3,46 +3,45 @@
 gnuplot << EOF
 
 set terminal epslatex size 7,5 standalone color colortext 10
-set output "Tmu_plot.tex"
+set output "psip2psi_s.tex"
 
 set border linewidth 1.5
-# Set first two line styles to blue (#0060ad) and red (#dd181f)
-set style line 1 linecolor rgb '#e41a1c' linetype 1 linewidth 3
-set style line 2 linecolor rgb '#377eb8' linetype 1 linewidth 3 
-set style line 3 linecolor rgb '#4daf4a' linetype 1 linewidth 3 
+
+set style line 1 linecolor rgb '#4daf4a' linetype 1 linewidth 3
+set style line 2 linecolor rgb '#4daf4a' linetype 0 linewidth 3
+set style line 3 linecolor rgb '#984ea3' linetype 1 linewidth 3 
+set style line 4 linecolor rgb '#984ea3' linetype 0 linewidth 3 
 
 set xlabel '\(\sqrt{s_{NN}}\;\left[\mathrm{GeV}\right]\)'
-set ylabel '\(\left[\mathrm{GeV}\right]\)'
+set ylabel 'Relative production \(\psi(2S)/J/\psi\)'
 
-set key at graph 0.8, graph 0.95
+set label 'Pb-Pb' at graph 0.1, graph 0.5
 
-set xrange [2:12000]
-set yrange [-.01:0.3]
+set key at graph 0.7, graph 0.45
+
+set xrange [10:12000]
+set yrange [0.:0.05]
 
 set logscale x
 set format x '\(10^{%L}\)'
 
-set grid
+plot "psip2psi_s_therm.dat" u 1:2 title 'This work' w l ls 1, \
+     "" u 1:3 notitle w l ls 2, \
+     "" u 1:4 notitle w l ls 2, \
+     "psip2psi_s_shm.dat" u 1:2 title 'Statistical hadronisation model' w l ls 3, \
+     "" u 1:3 notitle w l ls 4, \
+     "" u 1:4 notitle w l ls 4, \
+     "psip2psi_s_NA50.dat" u 1:2:3:4 notitle with yerrorbars linewidth 2 pointsize 2
 
-f(x) = (x > 3 && x <= 10000) ? 0.1584/(1+exp(2.6-log(x)/0.45)) : 1/0
-g(x) = (x > 3 && x <= 10000) ? 1.3075/(1+0.288*x) : 1/0
-
-# set style fill transparent solid 0.2 noborder
-
-#plot "s_md_error.dat" using 1:3:4 title "" with filledcurves, \
-
-
-# Plot
-plot f(x) title '\(T_{\mathrm{chem}}\)' w l ls 1, \
-     g(x) title '\(\mu_{\mathrm{chem}}\)' w l ls 2, \
-     "s_md_error.dat" using 1:2 title '\(m_D\)' w l ls 3
-
+# NOTE the NA50 results come from 0612013 last column in Table 3, combined with
+# di-muon deacy widths from PDG: J/Psi=0.05961, Psi'=8e-3
 
 EOF
 
-latex "Tmu_plot.tex"
-dvips -o "Tmu_plot.ps" "Tmu_plot.dvi"
+latex "psip2psi_s.tex"
+dvips -o "psip2psi_s.ps" "psip2psi_s.dvi"
 rm *.dvi *.aux *.log
 rm *.tex *.eps
-ps2pdf -dEPSCrop "Tmu_plot.ps" "Tmu_plot.pdf"
-rm "Tmu_plot.ps"
+ps2pdf -dEPSCrop "psip2psi_s.ps" "psip2psi_s.pdf"
+rm "psip2psi_s.ps"
+open "psip2psi_s.pdf"
