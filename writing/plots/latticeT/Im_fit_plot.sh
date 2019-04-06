882f51cbb550e47ebad9b90ac9fe45f81a68c161
David Lafferty <lafferty@thphys.uni-heidelberg.de>
2019-04-06 15:15:52 -0500

    Update

--- writing/plots/latticeT/Im_fit_plot.sh
+++ writing/plots/latticeT/Im_fit_plot.sh
@@ -2,49 +2,56 @@
 
 gnuplot << EOF
 
-set terminal pdfcairo enhanced color size 7,5 font 'CMU Serif,25'
+set terminal pdfcairo enhanced color size 7,7 font 'CMU Serif,45'
 set output "Im_fit.pdf"
-
-set lmargin 10
-set bmargin 4
 
 set encoding utf8
 
-set border linewidth 1.5
+set border 3 linewidth 4
+set tics nomirror
 
+set arrow from graph 1,0 to graph 1.05,0 filled linewidth 4
+set arrow from graph 0,1 to graph 0,1.05 filled linewidth 4
+
+set xrange [0:7.9]
+set xtics 1
+set xtics offset 0,graph 0.03
 set yrange [0:0.5]
+set ytics 0.1
 
-set xlabel 'r [fm]' 
-set ylabel '[GeV]'
-
-set key at graph 0.8, graph 0.3     
-set key spacing 1
+set label 'r [fm]' at graph 0.92,0.06
+set label 'ImV [GeV]' at graph 0.02,1.05
 
 set style fill transparent solid 0.3 noborder
 
-plot "../../../mathematica/DL_form/reg_pot/ImVlatT1.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#e41a1c',\
+set key opaque
+set key horizontal
+set key samplen 1
+set key width 1
+set key at graph 1.02,graph 1.01
+
+plot "../../../mathematica/DL_form/reg_pot/ImVlatT1.dat" u 1:2:3 title "148 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#e41a1c',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT1.dat" u 1:3:4 notitle w filledcurves lc rgb '#e41a1c', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#e41a1c', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT2.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#377eb8',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#e41a1c', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT2.dat" u 1:2:3 title "164 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#377eb8',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT2.dat" u 1:3:4 notitle w filledcurves lc rgb '#377eb8', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#377eb8', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT3.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#4daf4a',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#377eb8', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT3.dat" u 1:2:3 title "181 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#4daf4a',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT3.dat" u 1:3:4 notitle w filledcurves lc rgb '#4daf4a', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#4daf4a', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT4.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#984ea3',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#4daf4a', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT4.dat" u 1:2:3 title "205 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#984ea3',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT4.dat" u 1:3:4 notitle w filledcurves lc rgb '#984ea3', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#984ea3', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT5.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#ff7f00',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#984ea3', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT5.dat" u 1:2:3 title "232 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#ff7f00',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT5.dat" u 1:3:4 notitle w filledcurves lc rgb '#ff7f00', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#ff7f00', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT6.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#a65628',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#ff7f00', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT6.dat" u 1:2:3 title "242 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#a65628',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT6.dat" u 1:3:4 notitle w filledcurves lc rgb '#a65628', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#a65628', \
-     "../../../mathematica/DL_form/reg_pot/ImVlatT7.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#f781bf',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#a65628', \
+     "../../../mathematica/DL_form/reg_pot/ImVlatT7.dat" u 1:2:3 title "286 MeV" w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#f781bf',\
      "../../../mathematica/DL_form/reg_pot/ImVlatInterT7.dat" u 1:3:4 notitle w filledcurves lc rgb '#f781bf', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#f781bf'
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#f781bf'
 
 EOF
 
 open "Im_fit.pdf"
-
--- writing/plots/latticeT/Re_fit_plot.sh
+++ writing/plots/latticeT/Re_fit_plot.sh
@@ -2,54 +2,55 @@
 
 gnuplot << EOF
 
-set terminal pdfcairo enhanced color size 7,5 font 'CMU Serif,25'
+set terminal pdfcairo enhanced color size 7,7 font 'CMU Serif,45'
 set output "Re_fit.pdf"
-
-set lmargin 10
-set bmargin 4
 
 set encoding utf8
 
-set border linewidth 1.5
+set border 3 linewidth 4
+set tics nomirror
+
+set arrow from graph 1,0 to graph 1.05,0 filled linewidth 4
+set arrow from graph 0,1 to graph 0,1.05 filled linewidth 4
 
 set xrange [0:1.8]
-set yrange [0:8]
+set xtics 0.5
+set xtics offset 0,graph 0.03
+set yrange [0:7.9]
+set ytics 2
 
-set xlabel 'r [fm]' 
-set ylabel '[GeV]'
-
-set key at graph 0.8, graph 0.3     
-set key spacing 1
+set label 'r [fm]' at graph 0.92,0.06
+set label 'ReV [GeV]' at graph 0.02,1.05
 
 set style fill transparent solid 0.3 noborder
 
-plot "../../../mathematica/DL_form/reg_pot/ReVlatT1.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#e41a1c',\
+plot "../../../mathematica/DL_form/reg_pot/ReVlatT1.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#e41a1c',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT1.dat" u 1:3:4 notitle w filledcurves lc rgb '#e41a1c', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#e41a1c', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT2.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#377eb8',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#e41a1c', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT2.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#377eb8',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT2.dat" u 1:3:4 notitle w filledcurves lc rgb '#377eb8', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#377eb8', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT3.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#4daf4a',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#377eb8', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT3.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#4daf4a',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT3.dat" u 1:3:4 notitle w filledcurves lc rgb '#4daf4a', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#4daf4a', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT4.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#984ea3',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#4daf4a', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT4.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#984ea3',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT4.dat" u 1:3:4 notitle w filledcurves lc rgb '#984ea3', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#984ea3', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT5.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#ff7f00',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#984ea3', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT5.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#ff7f00',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT5.dat" u 1:3:4 notitle w filledcurves lc rgb '#ff7f00', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#ff7f00', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT6.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#a65628',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#ff7f00', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT6.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#a65628',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT6.dat" u 1:3:4 notitle w filledcurves lc rgb '#a65628', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#a65628', \
-     "../../../mathematica/DL_form/reg_pot/ReVlatT7.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#f781bf',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#a65628', \
+     "../../../mathematica/DL_form/reg_pot/ReVlatT7.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#f781bf',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT7.dat" u 1:3:4 notitle w filledcurves lc rgb '#f781bf', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#f781bf',\
-     "../../../mathematica/DL_form/reg_pot/ReVlatT01.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#999999',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#f781bf',\
+     "../../../mathematica/DL_form/reg_pot/ReVlatT01.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#999999',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT01.dat" u 1:3:4 notitle w filledcurves lc rgb '#999999', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#999999',\
-     "../../../mathematica/DL_form/reg_pot/ReVlatT02.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 3 pointsize 1 lc rgb '#999999',\
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#999999',\
+     "../../../mathematica/DL_form/reg_pot/ReVlatT02.dat" u 1:2:3 notitle w yerrorbars lt 7 lw 4 pointsize 1 lc rgb '#999999',\
      "../../../mathematica/DL_form/reg_pot/ReVlatInterT02.dat" u 1:3:4 notitle w filledcurves lc rgb '#999999', \
-     "" u 1:2 notitle w l lt 1 lw 2 lc rgb '#999999'
+     "" u 1:2 notitle w l lt 1 lw 4 lc rgb '#999999'
 
 EOF
 
